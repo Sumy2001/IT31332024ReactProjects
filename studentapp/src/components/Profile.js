fcd79175a5bf34bb4db373e1840a1ef48da5987f
Sumy2001 <154615028+Sumy2001@users.noreply.github.com>
2024-11-24 10:01:14 +0000

    Profile component added

---
+++ studentapp/src/components/Profile.js
@@ -0,0 +1,14 @@
+export default function Profile(props) {
+    return(
+        <>
+        <tablel>
+            <tr>
+                <td rowspan={7}>
+                    <img src={require('../assests/img/1.jpg')} alt="profile picture" />
+                </td>
+                
+            </tr>
+        </tablel>
+        </>
+    );
+}
