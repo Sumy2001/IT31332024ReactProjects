4d181de738c13fdeda517b5dd6149c3bf20070ef
Sumy2001 <154615028+Sumy2001@users.noreply.github.com>
2024-11-24 10:32:50 +0000

    view button click fun added

--- studentapp/src/components/Profile.js
+++ studentapp/src/components/Profile.js
@@ -1,14 +1,14 @@
 export default function Profile(props) {
     return(
         <>
-        <tablel>
+        <table>
             <tr>
                 <td rowspan={7}>
-                    <img src={require('../assests/img/1.jpg')} alt="profile picture" />
+                    <img className='profile' src={require('../assests/img/1.jpg')} alt="profile picture" />
                 </td>
-                
+                <td> ID {props.stu.studentId}</td>
             </tr>
-        </tablel>
+        </table>
         </>
     );
 }
